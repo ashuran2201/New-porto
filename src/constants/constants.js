71dd8aab9793c97a7ca3f1dedaad22029a583a3c
ashuran2201 <sultannaufal75@gmail.com>
2021-10-16 12:24:59 +0700

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -10,22 +10,22 @@ export const projects = [
   },
 
   {
-    title: 'React Porto page',
-    description: "A portofolio page which created to test react-router-dom",
-      image: '/images/3.png',
-      tags: ['React'],
-    source: 'https://react-personal-site-taupe.vercel.app/',
-    visit: 'https://github.com/ashuran2201/React-Personal-Site',
+    title: 'Sliding Sign In/Out',
+    description: "An animated Sign In/out",
+      image: '/images/3.PNG',
+      tags: ['HTML', 'CSS', 'Javascript'],
+    source: 'https://ashuran2201.github.io/sliding-sign-inup/',
+    visit: 'https://github.com/ashuran2201/sliding-sign-inup',
     id: 1,
   },
 
   {
-    title: 'Split Landing Page',
-    description: "This project creted with simple HTML, CSS and Javascript was created with the purpose of testing a split page landing page for my future web project",
-      image: '/images/1.png',
+    title: 'Animated Text',
+    description: "a project to make a text in website with animation",
+      image: '/images/1.PNG',
       tags: ['HTML', 'Css', 'Javascript'],
-    source: 'https://ashuran2201.github.io/split-landing-page/',
-    visit: 'https://github.com/ashuran2201/split-landing-page',
+    source: 'https://ashuran2201.github.io/animated-text/',
+    visit: 'https://github.com/ashuran2201/animated-text',
     id: 2,
   },
 
@@ -41,7 +41,6 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2019, text: 'Started my journey', },
-  { year: 2020, text: `Joined a Bootcamp By Glint's academy X Binar academy #9 Front-End Class`, },
+  { year: 2020, text: `Started my journey by joining a Bootcamp By Glint's academy X Binar academy #9 Front-End Class`, },
   { year: 2021, text: 'Job seeking while improving my capabilities' },
 ];
